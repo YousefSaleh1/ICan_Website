5eda93d18b7250cb41526768573c6c9f7d2ecb7b
Ayham Ibrahim <ayhamfacebook@gmail.com>
2023-12-22 00:16:53 +0300

    migrations

---
+++ ICan_Website/app/Models/Employee.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Photo;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Mosab\Translation\Database\TranslatableModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employee extends TranslatableModel
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+
+        'photo_id',
+        'facebook',
+        'linkedin',
+        'website',
+    ];
+    protected $translatable = [
+        'name',
+        'career',
+    ];
+
+     /**
+     * Get the user associated with the Employee
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function photo()
+    {
+        return $this->hasOne(Photo::class);
+    }
+}
---
+++ ICan_Website/app/Models/Slider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Photo;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Mosab\Translation\Database\TranslatableModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Slider extends TranslatableModel
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'photo_id',
+    ];
+
+    public function photo()
+    {
+        return $this->hasOne(Photo::class);
+    }
+}
