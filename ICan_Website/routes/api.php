57e38c7391a1c4e18943bbb251edf3a2fd852fc8
YousefSaleh1 <ys9882111@gmail.com>
2023-12-22 20:15:12 +0200

    Feedback Controller with apiRoute

--- ICan_Website/routes/api.php
+++ ICan_Website/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\DemandController;
+use App\Http\Controllers\API\FeedbackController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/demand_store', [DemandController::class, 'store']);
 
+Route::post('/feedback_store', [FeedbackController::class, 'store']);
+
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -27,5 +30,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/demands', [DemandController::class, 'index']);
     Route::get('/demand/{demand}', [DemandController::class, 'show']);
     Route::delete('/demand/{demand}', [DemandController::class, 'destroy']);
+
+    Route::get('/feedbacks', [FeedbackController::class, 'index']);
+    Route::get('/feedback/{feedback}', [FeedbackController::class, 'show']);
+    Route::delete('/feedback/{feedback}', [FeedbackController::class, 'destroy']);
 
 });
